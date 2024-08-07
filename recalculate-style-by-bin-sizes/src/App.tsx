0d59523aa609c02e304e503d4c98017778d310eb
Kristian Ekenes <kekenes@esri.com>
2024-08-07 14:59:19 -0700

    Add slider for changing bin level

--- recalculate-style-by-bin-sizes/src/App.tsx
+++ recalculate-style-by-bin-sizes/src/App.tsx
@@ -1,51 +1,75 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 import "@arcgis/map-components/dist/components/arcgis-map";
-import "@arcgis/map-components/dist/components/arcgis-expand";
 import "@arcgis/map-components/dist/components/arcgis-legend";
-import { ArcgisExpand, ArcgisLegend, ArcgisMap } from '@arcgis/map-components-react'
-import { defaultItemId, getUrlParams } from './loadData';
+import {
+  ArcgisLegend,
+  ArcgisMap,
+} from "@arcgis/map-components-react";
+import { defaultItemId, getUrlParams } from "./loadData";
 import esriConfig from "@arcgis/core/config";
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import "@esri/calcite-components/dist/components/calcite-slider";
-import { CalciteSlider } from '@esri/calcite-components-react';
+import { CalciteSlider } from "@esri/calcite-components-react";
 
 function App() {
   const mapRef = useRef<HTMLArcgisMapElement | null>(null);
-  const [webmapId, setWebmapId] = useState<string|undefined>(defaultItemId);
-  const [fixedBinLevel, setFixedBinLevel] = useState<number>(3);
-  const [layerId, setLayerId] = useState<number>(0);
+  const [webmapId, setWebmapId] = useState<string | undefined>(defaultItemId);
+  const [fixedBinLevel, setFixedBinLevel] = useState<number>(5);
+  const [layer, setLayer] = useState<__esri.FeatureLayer>(null!);
 
   const initialize = async () => {
+    const mapElement = mapRef.current;
+    if (!mapElement) return;
+
     const { id, portal, layerIndex } = getUrlParams();
     esriConfig.portalUrl = portal!;
     setWebmapId(id);
+
+    const selectedLayer = (
+      mapElement.map.layers.getItemAt(0) as __esri.GroupLayer
+    ).layers.getItemAt(layerIndex);
+    (mapElement.map.layers.getItemAt(0) as __esri.GroupLayer).layers.forEach(
+      (l, i) => (l.visible = i === layerIndex)
+    );
+    setLayer(selectedLayer as __esri.FeatureLayer);
   };
 
   useEffect(() => {
     console.log(fixedBinLevel);
-  }, [fixedBinLevel]);
+    if (!layer || !fixedBinLevel) {
+      return;
+    }
+
+    const featureReduction = (
+      layer.featureReduction as __esri.FeatureReductionBinning
+    ).clone();
+    featureReduction.fixedBinLevel = fixedBinLevel;
+    layer.featureReduction = featureReduction;
+  }, [fixedBinLevel, layer]);
 
   return (
     <>
-      <ArcgisMap ref={mapRef} itemId={webmapId} onArcgisViewReadyChange={initialize}>
-        <ArcgisExpand position='top-right' expanded={true}>
-          <div className='slider-content'>
-            <CalciteSlider
-              min={1}
-              max={9}
-              step={1}
-              value={fixedBinLevel}
-              onCalciteSliderChange={(e: any) => {
-                setFixedBinLevel(e.target.value);
-              }}
-            ></CalciteSlider>
-          </div>
-        </ArcgisExpand>
-        <ArcgisLegend position='bottom-left' />
+      <div className="slider-content">
+        <CalciteSlider
+          min={1}
+          max={9}
+          step={1}
+          value={fixedBinLevel}
+          onCalciteSliderChange={(e) => {
+            setFixedBinLevel(e.target.value as number);
+          }}
+        ></CalciteSlider>
+      </div>
+      <ArcgisMap
+        ref={mapRef}
+        itemId={webmapId}
+        onArcgisViewReadyChange={initialize}
+      >
+        <ArcgisLegend position="bottom-left" />
       </ArcgisMap>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
