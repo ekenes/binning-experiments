7e4e2ccfea1a149c32c51658a1145ba412638f0c
Kristian Ekenes <kekenes@esri.com>
2024-08-07 15:28:30 -0700

    chore: Refactor control layout and add regenerate option

--- recalculate-style-by-bin-sizes/src/App.tsx
+++ recalculate-style-by-bin-sizes/src/App.tsx
@@ -2,21 +2,25 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import "@arcgis/map-components/dist/components/arcgis-map";
 import "@arcgis/map-components/dist/components/arcgis-legend";
-import {
-  ArcgisLegend,
-  ArcgisMap,
-} from "@arcgis/map-components-react";
+import { ArcgisLegend, ArcgisMap } from "@arcgis/map-components-react";
 import { defaultItemId, getUrlParams } from "./loadData";
 import esriConfig from "@arcgis/core/config";
 import React, { useRef } from "react";
+import "@esri/calcite-components/dist/components/calcite-label";
 import "@esri/calcite-components/dist/components/calcite-slider";
-import { CalciteSlider } from "@esri/calcite-components-react";
+import "@esri/calcite-components/dist/components/calcite-switch";
+import {
+  CalciteLabel,
+  CalciteSlider,
+  CalciteSwitch,
+} from "@esri/calcite-components-react";
 
 function App() {
   const mapRef = useRef<HTMLArcgisMapElement | null>(null);
   const [webmapId, setWebmapId] = useState<string | undefined>(defaultItemId);
   const [fixedBinLevel, setFixedBinLevel] = useState<number>(5);
   const [layer, setLayer] = useState<__esri.FeatureLayer>(null!);
+  const [regenerateEnabled, setRegenerateEnabled] = useState<boolean>(false);
 
   const initialize = async () => {
     const mapElement = mapRef.current;
@@ -46,20 +50,43 @@ function App() {
     ).clone();
     featureReduction.fixedBinLevel = fixedBinLevel;
     layer.featureReduction = featureReduction;
-  }, [fixedBinLevel, layer]);
+
+    if (regenerateEnabled) {
+      // do something to regenerate the renderer
+    }
+
+  }, [fixedBinLevel, layer, regenerateEnabled]);
 
   return (
     <>
-      <div className="slider-content">
-        <CalciteSlider
-          min={1}
-          max={9}
-          step={1}
-          value={fixedBinLevel}
-          onCalciteSliderChange={(e) => {
-            setFixedBinLevel(e.target.value as number);
-          }}
-        ></CalciteSlider>
+      <div className="controls">
+        <h2>Binning regenerate style test</h2>
+        <div className="switch-content">
+          <CalciteLabel layout="inline" alignment="start">
+            Regenerate renderer
+            <CalciteSwitch
+              checked={regenerateEnabled}
+              onCalciteSwitchChange={(e) => {
+                setRegenerateEnabled(e.target.checked);
+              }}
+            />
+          </CalciteLabel>
+        </div>
+        <div className="slider-content">
+          <CalciteLabel layout="default">
+            Fixed Bin Level
+            <CalciteSlider
+              labelHandles={true}
+              min={1}
+              max={9}
+              step={1}
+              value={fixedBinLevel}
+              onCalciteSliderChange={(e) => {
+                setFixedBinLevel(e.target.value as number);
+              }}
+            ></CalciteSlider>
+          </CalciteLabel>
+        </div>
       </div>
       <ArcgisMap
         ref={mapRef}
